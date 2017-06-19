8f47afdf8d298ac49717eff0b395997a4e745f36
Tanner Welsh <tannerwelsh@gmail.com>
2017-06-19 15:39:59 -0400

    Complete routes for part 1

--- solutions/phase-3-challenge/part-1/server.js
+++ solutions/phase-3-challenge/part-1/server.js
@@ -5,6 +5,25 @@ app.get('/zero', (req, res) => {
   res.send('0')
 })
 
+app.get('/add', (req, res) => {
+  const {a, b} = req.query
+  const result = Number(a) + Number(b)
+  res.send(String(result))
+})
+
+
+app.get('/subtract', (req, res) => {
+  const {a, b} = req.query
+  const result = Number(a) - Number(b)
+  res.send(String(result))
+})
+
+app.get('/double/:number', (req, res) => {
+  const num = req.params.number
+  const result = Number(num) * 2
+  res.send(String(result))
+})
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000')
 })
