5ae522148d7df2f59c339ce3e9d882448113e000
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-11-27 14:13:30 -0600

    Add ingredient associations for pound cake and brownies.

--- foundations-to-practice/version-1/part-1/seed.sql
+++ foundations-to-practice/version-1/part-1/seed.sql
@@ -9,7 +9,7 @@ INSERT INTO recipes (name) VALUES
 INSERT INTO ingredients (name) VALUES
   ('flour'), -- 1
   ('sugar'), -- 2
-  ('vanilla'), -- 3
+  ('vanilla extract'), -- 3
   ('chocolate chips'), -- 4
   ('butter'), -- 5
   ('salt'), -- 6
@@ -26,5 +26,17 @@ INSERT INTO recipe_ingredients (recipe_id, ingredient_id, amount, amount_units) 
   (1, 8, 0.75, 'cup'),
   (1, 2, 0.75, 'cup'),
   (1, 6, 1, 'tsp'),
-  (1, 9, 2, '')
+  (1, 9, 2, ''),
+  (2, 5, 1, 'cup'),
+  (2, 1, 1, 'cups'),
+  (2, 2, 1, 'cup'),
+  (2, 9, 4, ''),
+  (2, 3, 2, 'tsps'),
+  (2, 6, 0.5, 'tsps'),
+  (3, 4, 1.5, 'cups'),
+  (3, 5, 2, 'tbs'),
+  (3, 2, 0.5, 'cups'),
+  (3, 1, 0.67, 'cups'),
+  (3, 7, 0.25, 'tsps'),
+  (3, 6, 0.5, 'tsps')
 ;
