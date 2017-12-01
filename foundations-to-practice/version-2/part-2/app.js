b57dc05f71e74baca19a29b85876c23575735d6a
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-12-01 13:21:36 -0800

    Fix typo in part-3 ejs. Add db.js JSDoc to app.js for part-2.

--- foundations-to-practice/version-2/part-2/app.js
+++ foundations-to-practice/version-2/part-2/app.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const { getFlightCounts } = require('./db/db')
+const express = require('express');
+const { getFlightCounts } = require('./db/db');
 
-const app = express()
+// JSDoc for getFlightCounts
+/**
+ * Get the names and flight counts for passengers having at least the minimum
+ * requested count
+ * @param  {number} minFlightCount Minimum number of flights for a passenger
+ *                                  to be included in results
+ * @return {Promise}              Promise that resolves to an array of objects.
+ *                                  Each object has keys 'name' and 'flightcount'
+ */
+
+const app = express();
 // app.set('view engine', 'pug')
 // app.set('view engine', 'ejs')
 
-app.listen(3000, () =>
-  console.log('Flying high on port 3000!')
-)
+app.listen(3000, () => console.log('Flying high on port 3000!'));
--- foundations-to-practice/version-2/part-3/app.js
+++ foundations-to-practice/version-2/part-3/app.js
@@ -1,23 +1,21 @@
-const express = require('express')
-const { getFlightCounts } = require('./db/db')
+const express = require('express');
+const { getFlightCounts } = require('./db/db');
 
-const app = express()
-app.use(express.static('public'))
-// app.set('view engine', 'pug')
-app.set('view engine', 'ejs')
+const app = express();
+app.use(express.static('public'));
+// app.set('view engine', 'pug');
+// app.set('view engine', 'ejs')
 
 app.get('/flight_counts', (req, res) => {
-  const minFlightCount = req.query.minFlightCount
+  const minFlightCount = req.query.minFlightCount;
   getFlightCounts(minFlightCount)
     .then((flightCounts) => {
-      res.render('flight_counts', { minFlightCount, flightCounts })
+      res.render('flight_counts', { minFlightCount, flightCounts });
     })
     .catch((err) => {
-      res.render('flight_counts', { message: `An error occurred: ${err.toString()}` })
+      res.render('flight_counts', { message: `An error occurred: ${err.toString()}` });
     })
-    .catch(console.error)
-})
+    .catch(console.error);
+});
 
-app.listen(3000, () =>
-  console.log('Flying high on port 3000!')
-)
+app.listen(3000, () => console.log('Flying high on port 3000!'));
